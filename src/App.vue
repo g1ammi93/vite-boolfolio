74639a3181c528971254059542da9810df99f532
g1ammi93 <giammariacarelli93@hotmail.it>
2024-04-01 18:19:22 +0100

    Added App and AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+
+const endpoint = 'http://localhost:8000/api/projects/'
 export default {
-  name: 'Boolfolio'
-}
+  name: 'Boolfolio',
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data);
+
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+};
 </script>
 
 <template>
-  <main class="container">
+  <AppHeader />
+  <main class="container pt-4">
     <h1>Boolfolio</h1>
+
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h4 v-text="project.title"></h4>
+      </li>
+    </ul>
   </main>
 </template>
 
